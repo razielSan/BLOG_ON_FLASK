1b0178164a665d89c34d85e6cca75172d5d853b6
raziel <tvindzond@gmail.com>
2023-06-19 15:58:36 +0700

    message 7

--- main.py
+++ main.py
@@ -9,5 +9,35 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/html')
+def html_page():
+    return render_template('html_page.html')
+
+
+@app.route('/django')
+def djano_page():
+    return render_template('django_page.html')
+
+
+@app.route('/css')
+def css_page():
+    return render_template('css_page.html')
+
+
+@app.route('/python')
+def python_page():
+    return render_template('python_page.html')
+
+
+@app.route('/js')
+def js_page():
+    return render_template('js_page.html')
+
+
+@app.route('/flask')
+def flask_page():
+    return render_template('flask_page.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
